c102144ef26fb45aa3e65368935b78d7187d870d
connor scoma <connorscoma@connors-MacBook-Pro.local>
2019-06-14 10:37:52 -0500

    sending to db

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,26 +1,35 @@
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyCaCRv6ytFs3rOkr2wSwhbmp2ikMEWlQDs",
-    authDomain: "train-db032.firebaseapp.com",
-    databaseURL: "https://train-db032.firebaseio.com",
-    projectId: "train-db032",
-    storageBucket: "train-db032.appspot.com",
-    messagingSenderId: "1074719918303",
-    appId: "1:1074719918303:web:781a2bbc17c20eb4"
-  };
-  // Initialize Firebase
-  
+  apiKey: "AIzaSyCaCRv6ytFs3rOkr2wSwhbmp2ikMEWlQDs",
+  authDomain: "train-db032.firebaseapp.com",
+  databaseURL: "https://train-db032.firebaseio.com",
+  projectId: "train-db032",
+  storageBucket: "train-db032.appspot.com",
+  messagingSenderId: "1074719918303",
+  appId: "1:1074719918303:web:781a2bbc17c20eb4"
+};
+// Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
   var database = firebase.database();
 
-  $("#submit").on("click", function(event) {
+  console.log(database)
+
+  $("#btn-add").on("click", function(event) {
   event.preventDefault();
 
-  var trainName = $("#train-name").val().trim();
+  var name = $("#train-name").val().trim();
   var destination = $("#train-destination").val().trim();
-  var trainTime = $("#train-time").val().trim();
+  var time = $("#train-time").val().trim();
   var frequency = $("#time-freq").val().trim();
 
+  var addingTrainToDB = {
+    name: name,
+    destination: destination,
+    time: time,
+    frequency: frequency
+  }
 
-  console.log(trainName)
+  database.ref().push(addingTrainToDB)
 
 })
